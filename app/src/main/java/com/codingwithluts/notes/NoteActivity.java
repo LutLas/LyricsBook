64a46046f50b4fe868b455a61bf1f7315472e83c
Muyoba Lutlas <lutakachihota@gmail.com>
2019-05-19 10:23:26 +0200

    Update NoteActivity.java

--- app/src/main/java/com/codingwithluts/notes/NoteActivity.java
+++ app/src/main/java/com/codingwithluts/notes/NoteActivity.java
@@ -3,12 +3,19 @@ package com.codingwithluts.notes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.codingwithluts.notes.models.Note;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements
+        View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener
+{
 
     private static final String TAG = "NoteActivity";
 
@@ -21,6 +28,8 @@ public class NoteActivity extends AppCompatActivity {
 
     // vars
     private boolean mIsNewNote;
+    private Note mNoteInitial;
+    private GestureDetector mGestureDetector;
 
 
     @Override
@@ -31,18 +40,26 @@ public class NoteActivity extends AppCompatActivity {
         mEditTitle = findViewById(R.id.note_edit_title);
         mViewTitle = findViewById(R.id.note_text_title);
 
+        setListeners();
+
         if(getIncomingIntent()){
             // this is a new note (EDIT MODE)
+            setNewNoteProperties();
         }
         else{
             // this is note a new note (VIEW MODE)
+            setNoteProperties();
         }
+    }
+
+    private void setListeners(){
+        mGestureDetector = new GestureDetector(this, this);
+        mLinedEditText.setOnTouchListener(this);
     }
 
     private boolean getIncomingIntent(){
         if(getIntent().hasExtra("selected_note")){
-            Note incomingNote = getIntent().getParcelableExtra("selected_note");
-
+            mNoteInitial = getIntent().getParcelableExtra("selected_note");
 
             mIsNewNote = false;
             return false;
@@ -51,6 +68,65 @@ public class NoteActivity extends AppCompatActivity {
         return true;
     }
 
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note Title");
+        mEditTitle.setText("Note Title");
+    }
 
+    private void setNoteProperties(){
+        mViewTitle.setText(mNoteInitial.getTitle());
+        mEditTitle.setText(mNoteInitial.getTitle());
+        mLinedEditText.setText(mNoteInitial.getContent());
+    }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return mGestureDetector.onTouchEvent(motionEvent);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        Log.d(TAG, "onDoubleTap: double tapped.");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
 }
